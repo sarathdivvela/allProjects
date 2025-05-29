f6a32569d3a6daa0ea43e462ae0322662896e541
sarath divvela <122167299+sarathdivvela@users.noreply.github.com>
2025-05-29 22:04:27 +0530

    Delete backup_files directory

--- backup_files/proctor-20240312T113653Z-001/proctor/test.py
+++
@@ -1,7 +0,0 @@
-from moviepy.editor import *
-
-# Load the WebM file
-webm_clip = VideoFileClip(r"C:\Users\GANESH\OneDrive\Documents\proctor-20240312T113653Z-001\proctor\sample.webm")
-
-# Write the clip to an MP4 file
-webm_clip.write_videofile("output.mp4", codec="libx264")
